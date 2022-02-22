a503adb3292afd8941d4baabec0b41a2b3732138
kuba <info@weblead.pl>
2022-02-22 13:36:20 +0100

    pierwszy

--- getProducts.php
+++ getProducts.php
@@ -46,7 +46,7 @@ function get_products_ids(){
             
           $needle =  array_search($category , $idArray);
             
-          if($needle != 'false'){
+          if($needle != false){
 
             var_dump($idArray[$needle]);
           }
