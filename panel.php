d6f7824057f1443a2dd729c894bb27ee303bcf45
kuba <info@weblead.pl>
2022-02-22 12:34:04 +0100

    pierwszy

--- panel.php
+++ panel.php
@@ -1,3 +1,35 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>A Basic HTML5 Template</title>
+  <meta name="description" content="XML generation panel">
+  <meta name="author" content="Jakub Troczyński">
+
+  <meta property="og:title" content="XML generation panel">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="A simple HTML5 Template for new projects.">
+  <meta property="og:image" content="image.png">
+
+  
+  
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="style.css">
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+
+<body>
+  <!-- your content here... -->
+  
+
+
+
 <?php
 
  $categoryArray['Kocot Kids Nowy']=801;
@@ -33,7 +65,7 @@
 
     </div>
 
- <div class="generuj" onclick="getValues()">
+ <div  class="generuj btn btn-primary"  onclick="getValues()">
  Generuj plik XMl
  </div>
 
@@ -63,3 +95,6 @@ function doRequest(string) {
   
 }
 </script>
+
+</body>
+</html>
