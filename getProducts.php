357132e96028c51c2e66d5c2cfc28b0989b89ba2
kuba <info@weblead.pl>
2022-02-22 13:02:19 +0100

    pierwszy

--- getProducts.php
+++ getProducts.php
@@ -1,23 +1,7 @@
 <?php
 
 
-$categoryArray['Kocot Kids Nowy']=801;
-$categoryArray['Comad']=712;
-$categoryArray['Wróbel']=716;
-$categoryArray['Akord']=741;
-$categoryArray['Piaski Skrzyniowe']=715;
-$categoryArray['Piaski Tapicerka']=886;
-$categoryArray['Platforma Mebli']=714;
-$categoryArray['Laski Skrzyniowe']=813;
-$categoryArray['Laski Tapicerka']=880;
-$categoryArray['Furnival']=713;
-$categoryArray['Prestiz']=867;
-$categoryArray['Vivaldi']=742;
-$categoryArray['Lemur']=796;
-$categoryArray['Marpol']=868;
-$categoryArray['Allegro']=788;
-$categoryArray['Katalog domyslny']=72;
-$categoryArray['Akord hurtownia']=1184;
+
 
 $q = $_REQUEST["q"];
 
