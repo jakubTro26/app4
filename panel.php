d9d3055dc9df1973000b312e79c5c52dc38be258
kuba <info@weblead.pl>
2022-02-23 12:13:00 +0100

    pierwszy

--- panel.php
+++ panel.php
@@ -116,7 +116,7 @@ function doRequest(string) {
         window.array = JSON.parse(xmlhttp.responseText);
 
 
-        handleCount(window.response);
+       // handleCount(window.response);
       }
     };
     xmlhttp.open("GET", "getIds.php?q=" + string, true);
